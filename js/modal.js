c680fe6729cfc4605696105d6eb931881d283006
Polina Tymchenko <polina0503800814@gmail.com>
2021-08-24 22:11:29 +0300

    adaptive design for page header added

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,27 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  refs.backdrop.addEventListener('click', backdropclick);
+  
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    window.addEventListener("keydown", handl);
+  }
+  function handl(e) {
+     if (e.code === "Escape") {
+       refs.modal.classList.toggle('is-hidden');
+       window.removeEventListener("keydown", handl);
+    }
+  }
+
+  function backdropclick(e) {
+    if (e.target !== e.currentTarget) return;
+    refs.modal.classList.toggle('is-hidden');
+   
   }
 })();
